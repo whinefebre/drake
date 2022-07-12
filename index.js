0a4b6fdd75c13b239446e8cf64984dff9e7225f0
whinefebre <whinemalajitofebre@gmail.com>
2022-07-12 20:03:22 +0800

    update
    Homepage,contact,portfolio and teams

--- index.js
+++ index.js
@@ -3,3 +3,30 @@ $(document).ready(function(){
         $(".site-nav-menu").toggleClass("mobile-menu");
     });
  });
+
+ //ACCORDION JS
+
+ $(document).ready(function() {
+    $('.acc-container .acc:nth-child(1) .acc-head').addClass('active');
+    $('.acc-container .acc:nth-child(1) .acc-content').slideDown();
+    $('.acc-head').on('click', function() {
+        if($(this).hasClass('active')) {
+          $(this).siblings('.acc-content').slideUp();
+          $(this).removeClass('active');
+        }
+        else {
+          $('.acc-content').slideUp();
+          $('.acc-head').removeClass('active');
+          $(this).siblings('.acc-content').slideToggle();
+          $(this).toggleClass('active');
+        }
+    });     
+});
+
+$("#slider").on("input change", (e)=>{
+  const sliderPos = e.target.value;
+  // Update the width of the foreground image
+  $('.foreground-img').css('width', `${sliderPos}%`)
+  // Update the position of the slider button
+  $('.slider-button').css('left', `calc(${sliderPos}% - 18px)`)
+});
